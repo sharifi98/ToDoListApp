b547ead2e90cdbc5caf15620d857af1959dd6a2d
Hossein Sharifi <47387238+sharifi98@users.noreply.github.com>
2023-12-15 20:31:33 +0100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Hossein Sharifi on 15/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
